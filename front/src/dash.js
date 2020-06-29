d61061f5d738f4bdc45553934c831e860dc55f27
AchmadRifai <officialrdevel@gmail.com>
2020-06-29 14:21:50 +0700

    carter

--- front/src/dash.js
+++ front/src/dash.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {makeStyles} from '@material-ui/styles'
+import {CssBaseline} from '@material-ui/core'
+import {Redirect} from 'react-router-dom'
+import Kepala from './dash/kepala'
 
 let useStyle=makeStyles(theme=>({
 	root: {
@@ -7,7 +10,11 @@ let useStyle=makeStyles(theme=>({
  	},
 }))
 
-export default function Dash(props) {
-	let gaya=useStyle()
-	return <div className={gaya.root}></div>
+export default function Dash() {
+	let gaya=useStyle(),[sesi,setSesi]=useState(localStorage.getItem('sesi'))
+	if(sesi)return<div className={gaya.root}>
+		<CssBaseline/>
+		<Kepala/>
+	</div>
+	else return<Redirect to='/login'/>
 }
